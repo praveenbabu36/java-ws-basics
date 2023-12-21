29f9d2c07801dd550933b0fb3d417d674459b130
praveenbabu36 <praveen.babu36@gmail.com>
2023-12-20 22:49:52 -0500

    Keywords | Keywords Example

---
+++ basics-1/src/com/parakalasuran/basics/ReservedKeywords_Ex_3.java
@@ -0,0 +1,22 @@
+package com.parakalasuran.basics;
+
+public class ReservedKeywords_Ex_3 {
+
+    public static void main(String[] args) {
+
+        boolean isHoliday = true;
+
+        int numOfDays = 30;
+
+
+
+    }
+}
+
+strictfp class calculatorEx {
+
+    float n1 = 10.23f;
+    float n2 = 20.45f;
+
+    
+}
