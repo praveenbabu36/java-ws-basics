4538f2ac6f9cab19b9362e2749f69c8b54fe3812
praveenbabu36 <praveen.babu36@gmail.com>
2023-12-20 20:24:14 -0500

    Comment | Different types of comments in java

--- basics-1/src/com/parakalasuran/basics/HelloWorld.java
+++ basics-1/src/com/parakalasuran/basics/HelloWorld.java
@@ -3,6 +3,20 @@ package com.parakalasuran.basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello Parakalasuran");
+
+        // Single line comment
+
+        /*
+            Multi line comment
+         */
+
+        /**
+         * Documentation Comment.
+         * Used for generating help docs
+         *
+         * @author : PKS
+         */
+
+        System.out.println("Hello Parakala Suran");
     }
 }
