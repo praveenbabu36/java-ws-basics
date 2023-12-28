2300c248176b184012e200fdc14438619f03a787
praveenbabu36 <praveen.babu36@gmail.com>
2023-12-28 10:38:10 -0500

    Conditional Statement | IF | IF...ELSE | Example

---
+++ basics-1/src/com/parakalasuran/basics/Conditional_Statements_Ex_11.java
@@ -0,0 +1,32 @@
+package com.parakalasuran.basics;
+
+public class Conditional_Statements_Ex_11 {
+
+    public static void main(String[] args) {
+
+        // Conditional Statements
+
+        int marks = 75;
+
+        // if
+        /*
+        if( marks > 80) {
+            System.out.println("You've achieved Class - I in the exam.");
+        }
+
+        System.out.println("Keep up good work.");
+        */
+
+        // if...else
+        if( marks > 80) {
+            System.out.println("You've achieved Class - I in the exam.");
+        } else {
+            System.out.println("You've achieved Class - II in the exam.");
+        }
+
+        System.out.println("Keep up good work.");
+
+
+    }
+
+}
