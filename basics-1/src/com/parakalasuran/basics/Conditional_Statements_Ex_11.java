7dcccc7697aa7850ac6eb277f567a95c0b9caa82
praveenbabu36 <praveen.babu36@gmail.com>
2023-12-28 12:50:45 -0500

    IF loop flavor.

--- basics-1/src/com/parakalasuran/basics/Conditional_Statements_Ex_11.java
+++ basics-1/src/com/parakalasuran/basics/Conditional_Statements_Ex_11.java
@@ -18,6 +18,7 @@ public class Conditional_Statements_Ex_11 {
         */
 
         // if...else
+        /*
         if( marks > 80) {
             System.out.println("You've achieved Class - I in the exam.");
         } else {
@@ -25,6 +26,24 @@ public class Conditional_Statements_Ex_11 {
         }
 
         System.out.println("Keep up good work.");
+        */
+
+        // if - expects the condition to return boolean
+        /*
+        boolean y = 10;
+        if( y = 0 ) {
+            System.out.println("y is 0");
+        }else{
+            System.out.println("y is not 0");
+        }
+        */
+
+        boolean x = true;
+        if( x = false ) {
+            System.out.println("x is false");
+        }else{
+            System.out.println("x is true");
+        }
 
 
     }
