512b4d994c33f22b66874edf99750a2815b3cfa8
Praveen <praveen.babu36@gmail.com>
2024-06-13 12:15:53 -0400

    Variables Example

---
+++ basics-1-2024/src/com/praveen/jdk21/basics/variables/Variable_Ex_1.java
@@ -0,0 +1,37 @@
+package com.praveen.jdk21.basics.variables;
+
+public class Variable_Ex_1 {
+
+    public static void main(String[] args) {
+
+        // Declaration
+        int variable1;
+
+        // Initialization
+        variable1 = 10;     //  cant use the variable without initialization
+
+        // print
+        System.out.println("value of variable1 :" + variable1);
+
+
+        // Declaration and Initialization together
+        int variable2 = 200;
+
+        // print
+        System.out.println("Value of variable2 :" + variable2);
+
+
+        // Declaration and initialization with arithmetic operation
+        int total = variable1 + variable2;
+
+        System.out.println("Total :"+ total);
+
+
+        // Reassigning new value to variable
+        variable2 = 100;
+
+        System.out.println("After reassignment. Value of variable2 :" + variable2);
+
+    }
+
+}
