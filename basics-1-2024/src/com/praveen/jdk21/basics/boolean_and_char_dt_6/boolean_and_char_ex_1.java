9ae4e679d36765f6d89ea279937793965d40339f
Praveen <praveen.babu36@gmail.com>
2024-07-02 12:07:45 -0400

    boolean and char datatype examples

---
+++ basics-1-2024/src/com/praveen/jdk21/basics/boolean_and_char_dt_6/boolean_and_char_ex_1.java
@@ -0,0 +1,22 @@
+package com.praveen.jdk21.basics.boolean_and_char_dt_6;
+
+public class boolean_and_char_ex_1 {
+
+    public static void main(String[] args) {
+
+        // boolean
+        boolean isBook = true;
+        System.out.println("value of isBook: " + isBook);
+
+        // char
+        //   only one char can be saved here
+        //   unicode can also be saved
+        char c1 = 'Z';
+        System.out.println("Value of c1: " + c1);
+
+        //   unicode can also be saved in char
+        char c2 = '\u0021';
+        System.out.println("Value of c2: " + c2);
+
+    }
+}
